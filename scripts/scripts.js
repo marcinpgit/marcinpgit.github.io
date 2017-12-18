85b8ae2b2a2bae7afc45d4ad01a729135d8eeff8
Marcin Płużyński <marcinp2012@gmail.com>
2017-12-19 00:28:38 +0100

    smooth scrolling added and some minor changes in code

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+// smooth scrolling
+
+$(document).ready(function() {
+    var scrollLink = $(".scroll");
+
+    scrollLink.click(function(event) {
+        event.preventDefault();
+        $("body, html").animate({
+            scrollTop: $(this.hash).offset().top
+        }, 800 );
+    });
+});
