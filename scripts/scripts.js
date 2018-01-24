16f45ae611c693d941d78ff9db4b5675e47f8dac
Marcin Płużyński <marcinp2012@gmail.com>
2018-01-25 00:16:34 +0100

    smooth top scroll added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,21 @@
 AOS.init();
 
+const $page = $('body, html');
+const $scrollUpBttn = $('#go-up');
+
 $(document).ready(function() {
-    var scrollLink = $(".scroll");
+    var scrollLink = $('.scroll');
 
     scrollLink.click(function(event) {
         event.preventDefault();
-        $("body, html").animate({
-            scrollTop: $(this.hash).offset().top
-        }, 800 );
+        $page.animate({scrollTop: $(this.hash).offset().top}, 800 );
     });
 });
+
+
+function goUp() {
+    $page.animate({scrollTop: 0}, 800);
+}
+
+$scrollUpBttn.on('click', goUp);
 
